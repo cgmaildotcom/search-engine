4daf5136dd1c0b6986b1fe78308c083d2df3d8b9
cgmaildotcom <cgmaildotcom@gmail.com>
2026-04-23 18:08:05 +0200

    Update: stemming algo was created

--- src/main/java/searchengine/secgmaildotcom/util/StemmerUtil.java
+++ src/main/java/searchengine/secgmaildotcom/util/StemmerUtil.java
@@ -1,5 +1,33 @@
 package searchengine.secgmaildotcom.util;
 
+import org.tartarus.snowball.ext.EnglishStemmer;
+
 public class StemmerUtil {
     
+    private static final EnglishStemmer englishStemmer = new EnglishStemmer();
+
+    public static String[] stem(String request) {
+        if (request == null || request.isEmpty()) {
+            return new String[0];
+        }
+        
+        String preparingRequestToStemming = request.toLowerCase()
+                                        .replaceAll("[^a-z0-9 ]", " ")
+                                        .replaceAll("\\s+", " ")
+                                        .trim();
+
+        String[] preparedRequestToStem = preparingRequestToStemming.split(" ");
+        String[] result = new String[preparedRequestToStem.length];
+
+        synchronized(englishStemmer) {
+            for (int i = 0; i < result.length; i++) {
+                englishStemmer.setCurrent(preparedRequestToStem[i]);
+                englishStemmer.stem();
+                result[i] = englishStemmer.getCurrent();
+            } 
+        }
+
+        return result;
+    }
+
 }
