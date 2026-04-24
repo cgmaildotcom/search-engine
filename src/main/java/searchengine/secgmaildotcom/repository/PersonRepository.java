60b48ea01dc2f6a4080ab740340c7d6e15c18cad
cgmaildotcom <cgmaildotcom@gmail.com>
2026-04-24 17:51:55 +0200

    Delete: unuseful file

--- src/main/java/searchengine/secgmaildotcom/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package searchengine.secgmaildotcom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import searchengine.secgmaildotcom.entity.PersonalityEntity;
-
-public interface PersonRepository extends JpaRepository <PersonalityEntity, Long> {  
-} 
